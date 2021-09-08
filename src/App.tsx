f6ec166e2bc14e4ad5457e4a2bb8bd57706a2911
Saul Florez <me@saulflorez.com>
2021-09-07 22:25:20 -0400

    🎉INIT: Iniciando proyecto

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Countries app</p>
+        <a
+          className="App-link"
+          href="https://countries.trevorblades.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          API GQL
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
