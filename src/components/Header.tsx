c1b248dc9c87e64786235a4f5ebd8ebf7c837763
Saul Florez <sflorez@globaltask.net>
2021-09-12 18:39:16 -0400

    🐛 FIX: Responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <header className="shadow py-5 px-12 bg-white">
+    <header className="shadow py-4 px-3 lg:py-5 lg:px-12 bg-white">
       <h2 className="text-xl">Countries App</h2>
     </header>
   );
