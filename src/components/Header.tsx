8b300fa491fa062b950912ab151b3263dc8de850
Saul Florez <sflorez@globaltask.net>
2021-09-15 12:59:10 -0400

    🐛 FIX: Nombre correcto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="shadow py-4 px-6 lg:py-5 lg:px-24 bg-white">
-      <h2 className="text-xl">Countries App</h2>
+      <h2 className="text-xl">Country App</h2>
     </header>
   );
 }
