86fd912e370eee7be2a4a4c88ea7d9e43daa7501
Sherzo <sherzo.florez@gmail.com>
2023-05-08 18:08:43 -0400

    📦 NEW: Emoji logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 export default function Header() {
   return (
     <header className="shadow py-4 px-6 lg:py-5 lg:px-24 bg-white">
-      <h2 className="text-xl">Country App</h2>
+      <h2 className="text-xl">🌎 Country App</h2>
     </header>
   );
 }
