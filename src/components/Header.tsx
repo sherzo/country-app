5ea358a014f2b539c5c265a04bce3bba04907e62
Sherzo <sherzo.florez@gmail.com>
2023-05-08 18:40:50 -0400

    👌 IMPROVE: Navegación desde el header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header className="shadow py-4 px-6 lg:py-5 lg:px-24 bg-white">
-      <h2 className="text-xl">🌎 Country App</h2>
+      <Link to={'/'}>
+        <h2 className="text-xl">🌎 Country App</h2>
+      </Link>
     </header>
   );
 }
